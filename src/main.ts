ac6532eaa76bfbf82120a8fbc1619af3464c6d1c
jonathanCocuy <jonathancp145@gmail.com>
2023-02-14 18:07:57 -0500

    segundo avance

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,28 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import 'zone.js/dist/zone';
+import {Component} from '@angular/core';
+import {bootstrapApplication} from '@angular/platform-browser';
 
-import { AppModule } from './app/app.module';
+@Component({
+  selector: 'app-contador',
+  standalone: true,
+  template:  `
+  <div class="div-principal">
+    <div class="contador">
+      <h1>Contador</h1> <br>
+        <div class="botones-ad"> 
+          <button class="button" (click)="count = count + 1">Aumentar uno</button>
+          <button class="button" (click)="count = count - 1">Disminuir uno</button>
+        </div>
+        <div class="count">{{ count }}</div>
+      <button class="button" (click)="count = 0">Reset</button>
+    </div>
+  </div>
+  `,
+ styleUrls: ['./styles.css']
+})
 
+export class AddOneButtonComponent {
+  count = 0;
+}
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AddOneButtonComponent);
